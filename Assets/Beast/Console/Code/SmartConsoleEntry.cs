81ac83da864a79fbedae12c8e29606503acd2637
pointcache <antonov.3d@gmail.com>
2017-01-07 23:42:43 -0500

    Overhaul complete, now only polish

---
+++ Assets/Beast/Console/Code/SmartConsoleEntry.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SmartConsoleEntry : MonoBehaviour
+{
+
+    public Text entryText;
+
+    public void SetText(string text)
+    {
+        entryText.text = text;
+    }
+
+    public void Clear()
+    {
+        entryText.text = "";
+    }
+}
